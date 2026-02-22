6b2767659c3b1f7bdcd0d531e840eefc80665f17
EmersonBerido <beridoemerson05@gmail.com>
2026-02-22 15:25:33 -0800

    Main Menu & Build Profile

--- Fading Code/Assets/Scripts/FinishLine.cs
+++ Fading Code/Assets/Scripts/FinishLine.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
+
+    public int nextSceneIndex;
+
+    public void NextLevel(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
---
+++ Fading Code/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    
+}
